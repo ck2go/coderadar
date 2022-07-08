037b69fb30df33af13b70c0e623c9c88df233da2
Carsten König <koenig@mpifr-bonn.mpg.de>
2022-07-08 18:43:37 +0200

    Increased version to 0.1.1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='coderadar',
-      version='0.1.0',
+      version='0.1.1',
       packages=find_packages(),
       entry_points = {
           'console_scripts': ['coderadar=coderadar.__main__:main'],
