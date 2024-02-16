5e6ba6a6341c158b3c144790fbbb653a37f7dc21
Carsten König <koenig@mpifr-bonn.mpg.de>
2024-02-16 14:28:47 +0100

    Added docstrings. Minor refactoring.

--- coderadar/report.py
+++ coderadar/report.py
@@ -6,19 +6,52 @@ from .pytest import CoverageReport
 
 
 class Report(object):
+    """
+    Class to summarize the code quality of a package.
+    """
     def __init__(self):
+        """
+        Initialize the report.
+        """
         pass
 
-
     def _getNum(self, num):
+        """
+        Converts a number to float or int if a number was given as string.
+
+        Parameters
+        ----------
+        num: object
+            The number as any type.
+
+        Returns
+        -------
+        int or float
+            The number.
+        """
         if type(num) is str:
             num = num[:-1] if num[-1] == '%' else num
             return float(num) if '.' in num else int(num)
 
         return num
 
-
     def _getDiffTxt(self, old, new):
+        """
+        Get the difference between two values in text format.
+
+        Parameters
+        ----------
+        old: object
+            The old value.
+
+        new: object
+            The new value.
+
+        Returns
+        -------
+        str
+            The difference in text format.
+        """
         if type(old) is str:
             postfix = '%' if old[-1] == '%' else ''
         else:
@@ -31,8 +64,24 @@ class Report(object):
             txt = ' (%+.2f%s)' % (new-old, postfix)
         return txt
 
-
     def _getDiffHtml(self, old, new, better=+1):
+        """
+        Get the difference between two values in HTML format.
+
+        Parameters
+        ----------
+        old: object
+            The old value.
+        new: object
+            The new value.
+        better:
+            The direction of improvement (+1: higher better; -1: lower better), or None.
+
+        Returns
+        -------
+        str
+            The difference in HTML format.
+        """
         if type(old) is str:
             postfix = '%' if old[-1] == '%' else ''
         else:
@@ -48,8 +97,15 @@ class Report(object):
             html = ' (<span style="color:%s">%+.2f%s</span>)' % (color, new - old, postfix)
         return html
 
-
     def _getReportTemplateTxt(self):
+        """
+        Get the report template in text format.
+
+        Returns
+        -------
+        str
+            The report template.
+        """
         template = """
 Code Quality Report
 <package_name>
@@ -87,8 +143,15 @@ Pylint Score:  <pylint_score>/10<pylint_score_diff>
 """
         return template
 
-
     def _getReportTemplateHtml(self):
+        """
+        Get the report template in HTML format.
+
+        Returns
+        -------
+        str
+            The report template.
+        """
         template = """<html><body>
 <head>
 <style>
@@ -143,16 +206,58 @@ Pylint Score:  <pylint_score>/10<pylint_score_diff>
 </body></html>"""
         return template
       
-      
     def getReportTemplate(self, report_type='txt'):
+        """
+        Get the report template.
+
+        Parameters
+        ----------
+        report_type: str
+            The report type ('txt' or 'html').
+
+        Returns
+        -------
+        str
+            The report template.
+        """
         if report_type.lower() == 'txt':
             template = self._getReportTemplateTxt()
         elif report_type.lower() == 'html':
             template = self._getReportTemplateHtml()
         return template
 
-
     def _replacePlacehlder(self, report, placeholder, actual_results, method_name, previous_results=None, arg=None, better=None):
+        """
+        Replace a placeholder in the report with the actual results.
+
+        Parameters
+        ----------
+        report: str
+            The report template.
+
+        placeholder: str
+            The placeholder to be replaced.
+
+        actual_results: object
+            The actual results e.g. for coverage or pylint.
+
+        method_name: str
+            The method to be called on results.
+
+        previous_results: object
+            The previous results (e.g. for coverage or pylint), or None.
+
+        arg: object
+            The argument to be passed to the method, or None.
+
+        better: int
+            The direction of improvement (+1: higher better; -1: lower better), or None.
+
+        Returns
+        -------
+        str
+            The filled report.
+        """
         if arg is None:
             actual_val = getattr(actual_results, method_name)()
         else:
@@ -173,8 +278,35 @@ Pylint Score:  <pylint_score>/10<pylint_score_diff>
                                         self._getDiffTxt(previous_val, actual_val))
         return report
 
+    def _fillTemplate(self, report, package_name, coverage, pylint, previous_coverage=None, previous_pylint=None):
+        """
+        Fill the report template with the actual results.
 
-    def _fillTemplate(self, report, package_name, coverage, pylint, previous_coverage, previous_pylint):
+        Parameters
+        ----------
+        report: str
+            The report template.
+
+        package_name: str
+            Name of the package to be analyzed.
+
+        coverage: CoverageReport
+            The coverage report.
+
+        pylint: PylintReport
+            The Pylint report.
+
+        previous_coverage: CoverageReport
+            The previous coverage report, or None.
+
+        previous_pylint: PylintReport
+            The previous Pylint report, or None.
+
+        Returns
+        -------
+        str
+            The filled report.
+        """
         report = report.replace('<package_name>', str(package_name))
         report = report.replace('<pytest_report_url>', str(coverage.getTxtUrl()))
 
@@ -183,63 +315,102 @@ Pylint Score:  <pylint_score>/10<pylint_score_diff>
         report = self._replacePlacehlder(report, 'pylint_score', pylint, 'getScore', previous_pylint, better=+1)
 
         report = report.replace('<pylint_report_url>', str(pylint.getTxtUrl()))
+
         report = self._replacePlacehlder(report, 'missing_docstrings', pylint, 'getMissingDocstrings', previous_pylint, better=-1)
+
         report = self._replacePlacehlder(report, 'too_complex_num', pylint, 'getTooComplex', previous_pylint, arg=0, better=-1)
         report = self._replacePlacehlder(report, 'too_complex_max', pylint, 'getTooComplex', previous_pylint, arg=1, better=-1)
         report = report.replace('<too_complex_file>', str(pylint.getTooComplex()[2]))
         report = report.replace('<too_complex_obj>', str(pylint.getTooComplex()[3]))
         report = report.replace('<too_complex_line>', str(pylint.getTooComplex()[4]))
+
         report = self._replacePlacehlder(report, 'func_too_long_num', pylint, 'getTooManyStatements', previous_pylint, arg=0, better=-1)
         report = self._replacePlacehlder(report, 'func_too_long_max', pylint, 'getTooManyStatements', previous_pylint, arg=1, better=-1)
         report = report.replace('<func_too_long_file>', str(pylint.getTooManyStatements()[2]))
         report = report.replace('<func_too_long_obj>', str(pylint.getTooManyStatements()[3]))
         report = report.replace('<func_too_long_line>', str(pylint.getTooManyStatements()[4]))
+
         report = self._replacePlacehlder(report, 'duplicate_code', pylint, 'getDuplicateCode', previous_pylint, arg=0, better=-1)
         report = report.replace('<duplicate_code_num>', str(pylint.getDuplicateCode()[1]))
         report = report.replace('<duplicate_code_files>', ', '.join(pylint.getDuplicateCode()[2]))
         report = report.replace('<duplicate_code_lines>', str(pylint.getDuplicateCode()[3]))
+
         report = self._replacePlacehlder(report, 'unused_imports', pylint, 'getUnusedImports', previous_pylint, arg=0, better=-1)
         report = report.replace('<unused_imports_file>', str(pylint.getUnusedImports()[1]))
         report = report.replace('<unused_imports_num>', str(pylint.getUnusedImports()[2]))
         report = report.replace('<unused_imports_items>', ', '.join(pylint.getUnusedImports()[3]))
+
         report = self._replacePlacehlder(report, 'unused_variables', pylint, 'getUnusedVariables', previous_pylint, arg=0, better=-1)
         report = report.replace('<unused_variables_file>', str(pylint.getUnusedVariables()[1]))
         report = report.replace('<unused_variables_num>', str(pylint.getUnusedVariables()[2]))
         report = report.replace('<unused_variables_items>', ', '.join(pylint.getUnusedVariables()[3]))
+
         report = self._replacePlacehlder(report, 'unused_arguments', pylint, 'getUnusedArguments', previous_pylint, arg=0, better=-1)
         report = report.replace('<unused_arguments_file>', str(pylint.getUnusedArguments()[1]))
         report = report.replace('<unused_arguments_num>', str(pylint.getUnusedArguments()[2]))
         report = report.replace('<unused_arguments_items>', ', '.join(pylint.getUnusedArguments()[3]))
+
         report = self._replacePlacehlder(report, 'unreachable_code', pylint, 'getUnreachableCode', previous_pylint, arg=0, better=-1)
         report = report.replace('<unreachable_code_file>', str(pylint.getUnreachableCode()[1]))
         report = report.replace('<unreachable_code_num>', str(pylint.getUnreachableCode()[2]))
         report = report.replace('<unreachable_code_items>', ', '.join(pylint.getUnreachableCode()[3]))
         return report
-    
-    
+
     def summarizeCodeQuality(self, package_name):
+        """
+        Summarize the code quality of the package.
+
+        Parameters
+        ----------
+        package_name: str
+            Name of the package to be analyzed.
+
+        Returns
+        -------
+        None
+        """
         coverage = CoverageReport()
-        previous_coverage_file = 'last_run/coverage'
-        if os.path.exists(previous_coverage_file+'.txt') and (os.path.getsize(previous_coverage_file+'.txt') > 0):
-            previous_coverage = CoverageReport(txt=previous_coverage_file+'.txt', xml=previous_coverage_file+'.xml')
-        else:
-            previous_coverage = None
+        previous_coverage = self._getPreviousCoverage()
 
         pylint = PylintReport()
+        previous_pylint = self._getPreviousPylint()
+
+        for report_type in ['txt', 'html']:
+            report = self.getReportTemplate(report_type=report_type)
+            report = self._fillTemplate(report, package_name, coverage, pylint, previous_coverage, previous_pylint)
+            if report_type == 'txt':
+                print(report)
+            with open('code_quality_report.%s' % report_type, 'w') as f:
+                f.write(report)
+
+    def _getPreviousPylint(self):
+        """
+        Get the previous Pylint report from file if it exists.
+
+        Returns
+        -------
+        PylintReport
+            The previous Pylint report.
+        """
         previous_pylint_file = 'last_run/pylint'
-        if os.path.exists(previous_pylint_file+'.txt') and (os.path.getsize(previous_pylint_file+'.json') > 0):
-            previous_pylint = PylintReport(txt=previous_pylint_file+'.txt', json=previous_pylint_file+'.json')
+        if os.path.exists(previous_pylint_file + '.txt') and (os.path.getsize(previous_pylint_file + '.json') > 0):
+            previous_pylint = PylintReport(txt=previous_pylint_file + '.txt', json=previous_pylint_file + '.json')
         else:
             previous_pylint = None
+        return previous_pylint
 
-        report_txt = self.getReportTemplate(report_type='txt')
-        report_txt = self._fillTemplate(report_txt, package_name, coverage, pylint, previous_coverage, previous_pylint)
-        print(report_txt)
-        with open('code_quality_report.txt', 'w') as f:
-            f.write(report_txt)
-        
-        report_html = self.getReportTemplate(report_type='html')
-        report_html = self._fillTemplate(report_html, package_name, coverage, pylint, previous_coverage, previous_pylint)
-        with open('code_quality_report.html', 'w') as f:
-            f.write(report_html)
-            
+    def _getPreviousCoverage(self):
+        """
+        Get the previous coverage report from file if it exists.
+
+        Returns
+        -------
+        CoverageReport
+            The previous coverage report.
+        """
+        previous_coverage_file = 'last_run/coverage'
+        if os.path.exists(previous_coverage_file + '.txt') and (os.path.getsize(previous_coverage_file + '.txt') > 0):
+            previous_coverage = CoverageReport(txt=previous_coverage_file + '.txt', xml=previous_coverage_file + '.xml')
+        else:
+            previous_coverage = None
+        return previous_coverage
